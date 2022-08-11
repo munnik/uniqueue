65e634467385ce39d226b5676365c909323874f1
munnik <martijndemunnik@protonmail.com>
2022-08-11 10:57:17 +0200

    Renamed variable

--- uq.go
+++ uq.go
@@ -35,22 +35,22 @@ func (u *UQ[T]) Front() <-chan T {
 	return u.front
 }
 
-func (u *UQ[T]) AddConstraint(value T) error {
+func (u *UQ[T]) AddConstraint(v T) error {
 	u.mu.Lock()
 	defer u.mu.Unlock()
 
-	if _, ok := u.constraints[value]; !ok {
-		u.constraints[value] = struct{}{}
+	if _, ok := u.constraints[v]; !ok {
+		u.constraints[v] = struct{}{}
 		return nil
 	}
 	return errors.New("Constraint already exists")
 }
 
-func (u *UQ[T]) RemoveConstraint(value T) {
+func (u *UQ[T]) RemoveConstraint(v T) {
 	u.mu.Lock()
 	defer u.mu.Unlock()
 
-	delete(u.constraints, value)
+	delete(u.constraints, v)
 }
 
 func (u *UQ[T]) linkChannels() {
@@ -64,10 +64,10 @@ func (u *UQ[T]) linkChannels() {
 }
 
 func (u *UQ[T]) shiftToFront(wg *sync.WaitGroup) {
-	for value := range u.queue {
-		u.front <- value
+	for v := range u.queue {
+		u.front <- v
 		if u.AutoRemoveConstraint {
-			u.RemoveConstraint(value)
+			u.RemoveConstraint(v)
 		}
 	}
 
@@ -77,9 +77,9 @@ func (u *UQ[T]) shiftToFront(wg *sync.WaitGroup) {
 }
 
 func (u *UQ[T]) readFromBack(wg *sync.WaitGroup) {
-	for value := range u.back {
-		if err := u.AddConstraint(value); err == nil {
-			u.queue <- value
+	for v := range u.back {
+		if err := u.AddConstraint(v); err == nil {
+			u.queue <- v
 		}
 	}
 
